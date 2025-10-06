c4bfff64a58de56ef6397d1bbd7bb78fecaf9ae3
Your Name <you@example.com>
2025-10-06 22:37:14 +0530

    init

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/products", productRoutes);
+app.get("/", (_req, res) => res.send("Hi from ashu"));
 
 // MongoDB Connection (no app.listen)
 mongoose.connect(process.env.MONGO_URI)
